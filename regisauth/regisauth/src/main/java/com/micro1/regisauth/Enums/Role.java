e14486bde512348e6d104e64c08d3955aa7a06a7
BATI <125739150+IBTISSAMLAKHOUIL@users.noreply.github.com>
2024-12-08 18:56:11 +0100

    Delete regisauth directory

--- regisauth/regisauth/src/main/java/com/micro1/regisauth/Enums/Role.java
+++
@@ -1,7 +0,0 @@
-package com.micro1.regisauth.Enums;
-
-public enum Role {
-    ETUDIANT,
-    LOUEUR,
-    ADMINISTRATEUR
-}
--- regisauth/regisauth/src/main/java/com/micro1/regisauth/Enums/Sexe.java
+++
@@ -1,6 +0,0 @@
-package com.micro1.regisauth.Enums;
-
-public enum Sexe {
-    HOMME,
-    FEMME
-}
